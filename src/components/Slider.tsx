35260c0d0b4dc3a3a7b9887f83ddd05bb9a664e2
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-31 23:56:36 +0900

    fix: svgr ver.4 방식으로 오류  수정

---
+++ src/components/Slider.tsx
@@ -0,0 +1,10 @@
+import Knob from "../assets/slider_knobs.svg?react";
+
+const Slider = () => {
+  return (
+    <div className="w-[142px] h-[4px] bg-black/0.05">
+      <Knob />
+    </div>
+  );
+};
+export default Slider;
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,4 @@
-declare module "*.svg" {
+declare module "*.svg?react" {
   import * as React from "react";
 
   export const ReactComponent: React.FunctionComponent<
